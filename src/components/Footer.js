1ddcf788ac38e6af23e35c3038647ac160dd4f13
YuliiaMatich <yuliia.matich@gmail.com>
2023-01-20 18:50:13 -0500

    image footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 return (
 <div className="footer">
   <img src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/call.png?raw=true"}></img>
-  <img src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/call.png?raw=true"}></img>
+  <img src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/user.png?raw=true"}></img>
   <img id="dialpad" src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/dialpad.png?raw=true"}></img>
   <img src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/gear.png?raw=true"}></img>
   <img src={"https://github.com/YuliiaMatich/aircall/blob/master/docs/images/record.png?raw=true"}></img>
